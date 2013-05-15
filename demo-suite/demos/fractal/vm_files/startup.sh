84896a28c43e54e1261a2e38dda502ab928c6cd2
Joe Beda <joe.github@bedafamily.com>
2013-05-15 06:13:04 -0700

    Switch to final Go 1.1 release for fractal demo.

--- demo-suite/demos/fractal/vm_files/startup.sh
+++ demo-suite/demos/fractal/vm_files/startup.sh
@@ -3,15 +3,18 @@ IMAGE_VERSION=2
 IMAGE_MARK=/var/fractal.image.$IMAGE_VERSION
 if [ ! -e $IMAGE_MARK ];
 then
-  curl -O https://go.googlecode.com/files/go1.1rc3.linux-amd64.tar.gz
-  tar -C /usr/local -xzf go1.1rc3.linux-amd64.tar.gz
+  pushd /tmp
+  curl -O https://go.googlecode.com/files/go1.1.linux-amd64.tar.gz
+  tar -C /usr/local -xzf go1.1.linux-amd64.tar.gz
   touch $IMAGE_MARK
+  popd
 fi
 
 export PATH=$PATH:/usr/local/go/bin
 GMV=/usr/share/google/get_metadata_value
 
 # Restart the server in the background if it fails.
+cd /tmp
 function runServer {
   while :
   do
