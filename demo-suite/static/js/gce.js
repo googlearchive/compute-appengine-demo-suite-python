3c15be4ec09dc2497ee8b0f3f03583865eb6e410
Adam Singer <financeCoding@gmail.com>
2013-09-09 19:16:36 -0700

    bug with Recovering not being defined and throwing js exception in chrome. This will follow the same pattern found else where in the code.

--- demo-suite/static/js/gce.js
+++ demo-suite/static/js/gce.js
@@ -264,7 +264,7 @@ Gce.prototype.heartbeat_ = function(numInstances, callback, terminalState) {
   // is complete), start the polling immediately to refresh state ASAP,
   // instead of waiting 2s to refresh the display.
   var that = this;
-  if ((typeof Recovering != undefined) && Recovering) {
+  if ((typeof Recovering !== 'undefined') && Recovering) {
     that.getStatuses_(success);
   } else {
     setTimeout(function() {
